e30bbe35aa88816d586c6221d2d20f485659bb08
serjga <serg862000@gmail.com>
2019-02-18 10:02:13 +0200

    homework lesson 1

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -74,6 +74,8 @@ class SiteController extends Controller
             return $this->goHome();
         }
 
+        $this->layout = 'admin-lte/main-login';
+
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
